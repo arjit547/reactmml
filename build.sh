f97c3521d31677d2c1bd8c227ca7dd214a43e8b9
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-12-19 10:01:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -22,3 +22,46 @@ cd ZAP_2.11.1
 ./zap.sh -cmd -quickurl https://www.example.com -quickprogress -quickout ../zap_report.html
 
 aws s3 cp ../zap_report.html s3://pipelinezapout/
+
+
+
+
+
+
+
+
+
+
+
+
+-------------------
+
+
+#!/bin/bash
+
+# Update package information
+apt-get update
+
+# Install wget
+apt-get -y install wget
+
+# Install default JDK
+apt-get -y install default-jdk
+
+# Download ZAP
+wget https://github.com/zaproxy/zaproxy/releases/download/v2.11.1/ZAP_2.11.1_Linux.tar.gz
+
+# Extract ZAP
+tar -xvf ZAP_2.11.1_Linux.tar.gz
+
+# Change directory to ZAP
+cd ZAP_2.11.1
+
+# Run ZAP command
+./zap.sh -cmd -quickurl https://www.example.com -quickprogress -quickout ../zap_report.html
+
+# Delete previous items from S3 bucket
+aws s3 rm s3://pipelinezapout/ --recursive
+
+# Upload the new report to S3
+aws s3 cp ../zap_report.html s3://pipelinezapout/

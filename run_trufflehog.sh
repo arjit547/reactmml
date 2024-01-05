86fbea18e2e70cbeef2d08e9a40284b71555c4ea
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-01-05 12:41:21 +0530

    Update run_trufflehog.sh

--- run_trufflehog.sh
+++ run_trufflehog.sh
@@ -10,7 +10,7 @@ pip install truffleHog
 git clone https://github.com/arjit547/reactmml.git
 
 # Run TruffleHog and save findings to a text file
-trufflehog --json --repo_path ./reactmml > trufflehog_findings.txt
+trufflehog --json --repo https://github.com/arjit547/reactmml.git > trufflehog_findings.txt
 
 # Upload TruffleHog findings (text file) to the TruffleHog S3 bucket
 aws s3 cp trufflehog_findings.txt s3://${TRUFFLEHOG_BUCKET}/trufflehog_findings.txt

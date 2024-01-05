117392c4b44223902b0ea7e3f7fe1bd934856dc6
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-01-05 12:12:00 +0530

    Update run_trufflehog.sh

--- run_trufflehog.sh
+++ run_trufflehog.sh
@@ -3,11 +3,8 @@
 # Specify the TruffleHog S3 bucket
 TRUFFLEHOG_BUCKET="tuffleoutstore"
 
-# Find the full path to the TruffleHog executable
-TRUFFLEHOG_PATH=$(which trufflehog)
-
 # Run TruffleHog and generate HTML report
-${TRUFFLEHOG_PATH} --repo https://github.com/arjit547/reactmml.git > trufflehog_output.txt
+${TRUFFLEHOG_PATH} find_secrets --repo https://github.com/arjit547/reactmml.git > trufflehog_output.txt
 sed 's/$/<br>/' trufflehog_output.txt > trufflehog_output.html
 
 # Upload TruffleHog HTML report to the TruffleHog S3 bucket

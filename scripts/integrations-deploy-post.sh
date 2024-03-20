ed21fcf2451ed0089dd2f9aaef6833c8437bc198
arjit547 <97424724+arjit547@users.noreply.github.com>
2024-03-20 10:23:42 +0530

    Update integrations-deploy-post.sh

--- scripts/integrations-deploy-post.sh
+++ scripts/integrations-deploy-post.sh
@@ -6,14 +6,14 @@ then
     then
         echo "Waiting for 2 minutes...."
         sleep 120
-        cp -R /home/my-temp-dir/. /var/www/html
+        cp -R /home/my-temp-dir/. /home/ubuntu/upload
         sudo rm -rf /home/my-temp-dir
-        chown -R ubuntu:ubuntu /var/www/html
-        cd /var/www/html
+        chown -R ubuntu:ubuntu /home/ubuntu/upload
         npm install
 
         # Run npm build and check its exit code
         npm run build
+        cp -r build * /var/www/html
 
         if [ $? -eq 0 ]
         then
@@ -22,14 +22,14 @@ then
             echo "Error: npm run build failed."
         fi
     else
-        cp -R /home/my-temp-dir/. /var/www/html
+        cp -R /home/my-temp-dir/. /home/ubuntu/upload
         sudo rm -rf /home/my-temp-dir
-        chown -R ubuntu:ubuntu /var/www/html
-        cd /var/www/html
+        chown -R ubuntu:ubuntu /home/ubuntu/upload
         npm install
 
         # Run npm build and check its exit code
         npm run build
+        cp -r build * /var/www/html
 
         if [ $? -eq 0 ]
         then
